adcfd58801fa0bfa5decbf515cffbd333a897548
Xi Chen <31968520+greinerchan@users.noreply.github.com>
2018-04-16 19:44:47 -0400

    Add files via upload

---
+++ Interrupt/include/peripherals.h
@@ -0,0 +1,40 @@
+/**
+ ****************************************************************************************
+ *
+ * @file peripherals.h
+ *
+ * @brief Peripherals initialization fucntions headers
+ *
+ * Copyright (C) 2012. Dialog Semiconductor Ltd, unpublished work. This computer 
+ * program includes Confidential, Proprietary Information and is a Trade Secret of 
+ * Dialog Semiconductor Ltd.  All use, disclosure, and/or reproduction is prohibited 
+ * unless authorized in writing. All Rights Reserved.
+ *
+ * <bluetooth.support@diasemi.com> and contributors.
+ *
+ ****************************************************************************************
+ */ 
+#include <stdint.h>
+
+#ifndef PERIPHERAL_H_INCLUDED
+#define PERIPHERAL_H_INCLUDED
+
+#define BUTTON2_PORT  GPIO_PORT_2
+#define BUTTON2_PIN   GPIO_PIN_3
+
+#define BUTTON3_PORT  GPIO_PORT_2
+#define BUTTON3_PIN   GPIO_PIN_4
+
+#define LED4_PORT    GPIO_PORT_2
+#define LED4_PIN     GPIO_PIN_1
+
+#define LED5_PORT    GPIO_PORT_2
+#define LED5_PIN     GPIO_PIN_2
+
+#define LED6_PORT    GPIO_PORT_0
+#define LED6_PIN     GPIO_PIN_7
+
+void set_pad_functions(void);        // set gpio port function mode
+void periph_init(void) ;
+ 
+#endif
---
+++ Interrupt/src/main.c
@@ -0,0 +1,52 @@
+
+ /*
+ ****************************************************************************************
+ *
+ * @file DA14580_examples.c
+ *
+ * @brief DA14580 Peripheral Examples for DA14580 SDK
+ *
+ * Copyright (C) 2012. Dialog Semiconductor Ltd, unpublished work. This computer 
+ * program includes Confidential, Proprietary Information and is a Trade Secret of 
+ * Dialog Semiconductor Ltd.  All use, disclosure, and/or reproduction is prohibited 
+ * unless authorized in writing. All Rights Reserved.
+ *
+ * <bluetooth.support@diasemi.com> and contributors.
+ *
+ ****************************************************************************************
+ */
+#include <stdio.h>
+#include "global_io.h"
+#include "peripherals.h"
+#include "gpio.h"
+
+void gpio_int(void)
+{
+  SetWord16(GPIO_IRQ0_IN_SEL_REG,0x0012);
+	SetWord16(GPIO_IRQ1_IN_SEL_REG,0x0013);
+	SetWord16(GPIO_DEBOUNCE_REG,0x0310);
+	SetWord16(GPIO_INT_LEVEL_CTRL_REG,0x0303);
+}
+
+int main (void)
+{	
+	periph_init();
+	gpio_int();
+	NVIC_SetPriority(GPIO0_IRQn,1);
+	NVIC_SetPriority(GPIO1_IRQn,2);
+	NVIC_EnableIRQ(GPIO0_IRQn);
+	NVIC_EnableIRQ(GPIO1_IRQn);
+	while(1);
+}
+
+void GPIO0_Handler(void)
+{
+	GPIO_SetActive(LED4_PORT, LED4_PIN);
+  SetWord16(GPIO_RESET_IRQ_REG,0x0001);
+}
+
+void GPIO1_Handler(void)
+{
+	GPIO_SetInactive(LED4_PORT, LED4_PIN);
+  SetWord16(GPIO_RESET_IRQ_REG,0x0002);
+}
